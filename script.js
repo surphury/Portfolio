5b97ebdee7debc2d05ce91e4714a256483482b76
Jose Manuel <jose@261004@gmail.com>
2021-10-01 20:10:56 -0400

    fix: the icon doesn't if your default theme is light

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 'use strict';
 
-const root = document.documentElement;
+const $root = document.documentElement;
 
-const $switcher = document.getElementById('switcher');
+const $switcher = document.getElementById('switcher'),
+    $cover = document.getElementById('cover');
 
-$switcher.addEventListener('click', function () {
-    this.classList.toggle('icon--moon');
-    if (root.toggleAttribute('light-theme'))
+const changeTheme = () => {
+    $switcher.classList.toggle('icon--moon');
+    
+    if ($root.classList.toggle('light-theme'))
         localStorage.setItem('light-theme', true);
     else
         localStorage.clear();
-});
+}
 
-if (localStorage.getItem('light-theme'))
-    root.setAttribute('light-theme', '');
+localStorage.getItem('light-theme') && changeTheme();
+
+
+$switcher.addEventListener('click', changeTheme);
+
+document.addEventListener('DOMContentLoaded', () => {
+    $cover.classList.add('cover--hidden');
+    $cover.addEventListener('animationend', () => $cover.remove());
+}, { once: true });
