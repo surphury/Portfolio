cf70f8d705c7a97a07253e36eb8550b18ee82748
Jose Manuel <jose@261004@gmail.com>
2021-10-04 13:16:18 -0400

    refactor: Move cover's scripts to another file

---
+++ js/cover.js
@@ -0,0 +1,6 @@
+const $cover = document.getElementById('cover');
+document.addEventListener('DOMContentLoaded', () => {
+    $cover.classList.add('cover--hidden');
+}, { once: true });
+
+$cover.addEventListener('animationend', () => $cover.remove());
